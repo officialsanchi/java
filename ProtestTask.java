2ccf28675dd32b43da850836b5039a7d53e0d761
officialsanchi <149580510+officialsanchi@users.noreply.github.com>
2024-08-03 21:34:20 +0100

    Add files via upload

---
+++ ProtestTask.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+public class ProtestTask{
+
+	public static void main(String [] args){
+	Random random = new Random();
+
+
+	int demand = random.nextInt(1, 14);
+
+	if(demand == 1)System.out.print("Scrap the 1999 Constitution  and replace it with a people _ made CONSTITUTION for the federal Republic of Nigeria through a sovereign National Conference to be immediately followed by a National Referendum ");
+
+	else if (demand == 2)System.out.print("Toss the Senate arm of the Nigeria Legislative System, keep the House of Respresentatives (HOR), and make lawmaking a part-time endeavour");
+
+	else if (demand == 3)System.out.print("Pay Nigeria Workers a minimum wage of nothing less than N250.000 monthly");
+
+	else if (demand == 4)System.out.print("Invest heavily in education and give Nigerian students grants, not loans. Aggressively pursue free and compulsory education for children across Nigeria.");
+
+	else if (demand == 5)System.out.print("Release Mazi Nnamdi Kanu unconditionally and demilitarize the South East.");
+
+	else if (demand == 6)System.out.print("Renationalise publicly owned enterprises sold to government officials and cronies.");
+
+	else if (demand == 7)System.out.print("Reinstate a corruption-free subsidy regime to reduce hunger, starvation and multidimensional poverty.");
+
+
+	else if (demand == 8)System.out.print("Probe past and present Nigerian leaders who have looted the treasury, recover their loot, and deposit it in a special account to fund education, healthcare, and infrastructure.");
+
+	else if (demand == 9)System.out.print("Restructuring of Nigeria to accommodate Nigeria’s diversity, resource control, decentralisation and regional development.");
+
+	else if (demand == 10)System.out.print("Reforms of Security Agencies to stop continuous human rights violations and duplication of security agencies and enhance the physical security of Nigerian citizens.");
+
+	else if (demand == 11)System.out.print("Establish a Special Energy Fund immediately to drive massive, corruption-free power sector development.");
+
+	else if (demand == 12)System.out.print("Immediate Reconstitution of the Nigerian Electoral Body, the Independent National Electoral Commission (INEC) to remove corrupt individuals and partisan hacks appointed to manipulate elections.");
+
+	else if (demand == 13)System.out.print("Massive investment in public works and industrialisation will help employ Nigeria’s teeming youths.");
+
+	else if (demand == 14)System.out.print("Massive shake-up in the Nigerian judiciary to remove cabals of corrupt generations of judges and judicial officers that continue denying everyday citizens access to real justice.”");
+
+
+
+	
+
+	
+
+
+
+		}
+
+	}
---
+++ protest.py
@@ -0,0 +1,52 @@
+import random
+
+demand = random. randint(1, 14)
+
+
+if(demand == 1):
+	print(" Scrap the 1999 Constitution and replace it with a people made CONSTITUTION for the federal Republic of Nigeria through a sovereign National Conference to be immediately followed by a National Referendum ")
+
+elif (demand == 2):
+	print("Toss the Senate arm of the Nigeria Legislative System, keep the House of Respresentatives (HOR), and make lawmaking a part-time endeavour")
+
+elif (demand == 3):
+	print("Pay Nigeria Workers a minimum wage of nothing less than N250.000 monthly")
+
+elif (demand == 4):
+	print("Invest heavily in education and give Nigerian students grants, not loans. Aggressively pursue free and compulsory education for children across Nigeria.")
+
+elif (demand == 5):
+	print("Release Mazi Nnamdi Kanu unconditionally and demilitarize the South East.")
+
+elif (demand == 6):
+	print("Renationalise publicly owned enterprises sold to government officials and cronies.")
+
+elif (demand == 7):
+	print("Reinstate a corruption-free subsidy regime to reduce hunger, starvation and multidimensional poverty.")
+elif (demand == 8):
+	print("Probe past and present Nigerian leaders who have looted the treasury, recover their loot, and deposit it in a special account to fund education, healthcare, and infrastructure.")
+
+elif (demand == 9):
+	print("Restructuring of Nigeria to accommodate Nigeria’s diversity, resource control, decentralisation and regional development.")
+
+elif (demand == 10):
+	print("Reforms of Security Agencies to stop continuous human rights violations and duplication of security agencies and enhance the physical security of Nigerian citizens.")
+
+elif (demand == 11):
+	print("Establish a Special Energy Fund immediately to drive massive, corruption-free power sector development.")
+
+elif (demand == 12):
+	print("Immediate Reconstitution of the Nigerian Electoral Body, the Independent National Electoral Commission (INEC) to remove corrupt individuals and partisan hacks appointed to manipulate elections.")
+
+elif (demand == 13):
+	print("Massive investment in public works and industrialisation will help employ Nigeria’s teeming youths.")
+
+elif (demand == 14):
+	print("Massive shake-up in the Nigerian judiciary to remove cabals of corrupt generations of judges and judicial officers that continue denying everyday citizens access to real justice.”")
+
+
+
+	
+
+	
+
